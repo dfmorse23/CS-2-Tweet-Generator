d007cb011d5bc938282c17243358393922d3bbd0
dfmorse23 <dfmorse23@gmail.com>
2017-11-07 14:28:02 -0800

    flask app stuff

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python.exe server_app
+web: python3 server.py
--- server.py
+++ server.py
@@ -1,6 +1,27 @@
+import os
 from flask import Flask
 the_app = Flask(__name__)
 
 @the_app.route('/')
 def hello_word():
     print("hellow world")
+
+if __name__ == "__main__":
+    # goes up to heroku server saved in the local enviornment
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+#
+# [[source]]
+# url = "http://127.0.0.1:5000/"
+# verify_ssl = true
+#
+# [packages]
+# dj-database-url = "*"
+# django = "*"
+# gunicorn = "*"
+# psycopg2 = "*"
+# whitenoise = "*"
+#
+# [requires]
+# python_version = "2.7.10"
